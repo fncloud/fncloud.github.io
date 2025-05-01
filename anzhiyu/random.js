8df81a4784477f946a12426b0fdf9ffce7d17bd2
fncloud <junkaiw720@gmail.com>
2025-05-01 18:02:49 +0800

    Site updated: 2025-05-01 18:02:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/30/hello-world/"];function toRandomPost(){
+var posts=["2025/05/01/科学上网后有哪些乐趣/","2025/04/30/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
